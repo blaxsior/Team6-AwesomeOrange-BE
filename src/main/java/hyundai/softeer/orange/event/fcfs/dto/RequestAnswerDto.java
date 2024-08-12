c069b5c180ed0e5db80326048a6d18bf45fca037
win-luck <chaso1495@naver.com>
2024-08-12 14:00:23 +0900

    [fix] 선착순 이벤트 관련 API 매개변수 수정 (#47)

---
+++ src/main/java/hyundai/softeer/orange/event/fcfs/dto/RequestAnswerDto.java
@@ -0,0 +1,12 @@
+package hyundai.softeer.orange.event.fcfs.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RequestAnswerDto {
+    private String answer;
+}
