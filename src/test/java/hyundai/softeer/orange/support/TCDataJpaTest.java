f7ee4ebd96f98585ab9837d8ae52895cc7c38c94
win-luck <chaso1495@naver.com>
2024-08-14 16:54:37 +0900

    [chore] 테스트 컨테이너 통합테스트 어노테이션 TCDataJpaTest 도입 (#64)

---
+++ src/test/java/hyundai/softeer/orange/support/TCDataJpaTest.java
@@ -0,0 +1,20 @@
+package hyundai.softeer.orange.support;
+
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@ActiveProfiles("test")
+@Testcontainers
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public @interface TCDataJpaTest {
+}
