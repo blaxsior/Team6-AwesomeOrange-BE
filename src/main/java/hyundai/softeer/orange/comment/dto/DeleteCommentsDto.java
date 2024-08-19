4a38fbaf4e2ca412a0fec7e24a4536a428dbd9d0
win-luck <chaso1495@naver.com>
2024-08-19 12:27:19 +0900

    [fix] 어드민 기대평 삭제 API 직렬화 에러 수정 및 캐시 초기화 (#89)

--- src/main/java/hyundai/softeer/orange/comment/dto/DeleteCommentsDto.java
+++ src/main/java/hyundai/softeer/orange/comment/dto/DeleteCommentsDto.java
@@ -2,10 +2,12 @@ package hyundai.softeer.orange.comment.dto;
 
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
 public class DeleteCommentsDto {
     @NotNull
     private List<Long> commentIds;
