a50384a3645e090792ca72f12c0357faab2b1e2b
blaxsior <heejun2798@gmail.com>
2024-08-20 18:20:40 +0900

    [feat] 이벤트에 이벤트 프레임 포함해서 반환하도록 변경  (#110)

--- src/main/java/hyundai/softeer/orange/event/common/repository/EventMetadataRepository.java
+++ src/main/java/hyundai/softeer/orange/event/common/repository/EventMetadataRepository.java
@@ -3,6 +3,8 @@ package hyundai.softeer.orange.event.common.repository;
 import hyundai.softeer.orange.event.common.entity.EventMetadata;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,4 +12,7 @@ import java.util.Optional;
 @Repository
 public interface EventMetadataRepository extends JpaRepository<EventMetadata, Long>, JpaSpecificationExecutor<EventMetadata> {
     Optional<EventMetadata> findFirstByEventId(String eventId);
+
+    @Query("SELECT e from EventMetadata e join fetch e.eventFrame WHERE e.eventId = :eventId")
+    Optional<EventMetadata> findByEventIdWithEventFrame(@Param("eventId") String eventId);
 }
